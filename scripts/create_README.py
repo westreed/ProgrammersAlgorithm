de86b52abeb8b5917bca621da7838d226c5a84cb
JangSeHun <westreed@naver.com>
2022-04-13 21:37:36 +0900

    update scripts

--- scripts/create_README.py
+++ scripts/create_README.py
@@ -12,8 +12,6 @@ tableHeader = ["난이도","문제 링크","해결한 문제 수","전체 문제
 tables.append(f"| {' | '.join(tableHeader)} |")
 tables.append(f"| {':--: |' * len(tableHeader)}")
 for idx in range(0, 4):
-    level = f'레벨{idx+1}'
-
     path = f'./lv{idx+1}'
     filelists = os.listdir(path)
     
@@ -23,7 +21,7 @@ for idx in range(0, 4):
         if file[-4:] == 'X.py':
             unresolved += 1
     
-    line = f'|{level}|[바로가기](https://github.com/westreed/ProgrammersAlgorithm/blob/main/LEVEL{idx+1}.md)|{problems-unresolved}|{problems}|'
+    line = f'|레벨{idx+1}|[바로가기](https://github.com/westreed/ProgrammersAlgorithm/blob/main/LEVEL{idx+1}.md)|{problems-unresolved:02}|{problems:02}|'
     tables.append(line)
 
 tables = [ f"{line}\n" for line in tables ]
