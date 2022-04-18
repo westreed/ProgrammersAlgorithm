951ee28c00c6dc60ae481148524ac93f26672cc1
JangSeHun <westreed@naver.com>
2022-04-18 22:48:49 +0900

    update scripts

--- scripts/create_README.py
+++ scripts/create_README.py
@@ -9,7 +9,6 @@ with open('./md/header.md', 'r', encoding = "UTF-8") as f:
     f.close()
 
 tableHeader = ["난이도","문제 링크","해결한 문제 수","전체 문제 수"]
-tables.append(f"-----------------------------")
 tables.append(f"## 프로그래머스")
 
 tables.append(f"| {' | '.join(tableHeader)} |")
@@ -30,7 +29,6 @@ for idx in range(4):
     line = f'|레벨{idx+1}|[바로가기]({links})|{problems-unresolved:02}|{problems:02}|'
     tables.append(line)
 
-tables.append(f"-----------------------------")
 tables.append(f"## 삼성 SW 아카데미")
 
 tables.append(f"| {' | '.join(tableHeader)} |")
@@ -51,7 +49,6 @@ for idx in range(1):
     line = f'|레벨{idx+1}|[바로가기]({links})|{problems-unresolved:02}|{problems:02}|'
     tables.append(line)
 
-tables.append(f"-----------------------------")
 tables = [ f"{line}\n" for line in tables ]
 
 # README.md
