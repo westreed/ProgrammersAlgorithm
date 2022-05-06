767c65c4aeab351513baf95a610cd46bbfdf2847
JangSeHun <westreed@naver.com>
2022-05-06 17:17:46 +0900

    문제 업로드

--- BAEKJOON/2Silver/체스판 다시 칠하기 X.py
+++ BAEKJOON/2Silver/체스판 다시 칠하기 X.py
@@ -1,4 +1,22 @@
 #
 # https://www.acmicpc.net/problem/1018
+from math import ceil
 
 M,N = map(int, input().split())
+Chess = []
+Pattern = []
+Checksum = []
+for m in range(M):
+    data = input().strip()
+    Chess.append(data)
+
+data = 'BW' * ceil(N/2)
+Pattern.append(data[:N])
+data = 'WB' * ceil(N/2)
+Pattern.append(data[:N])
+
+for i in range(2):
+    
+
+for m in range(M):
+    pass
