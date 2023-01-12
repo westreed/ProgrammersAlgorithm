13178a748bc6c9466f70c21071376366a65f7dcd
JangSeHun <westreed@naver.com>
2023-01-12 17:14:34 +0900

    data 옵션 추가

--- scripts/data.py
+++ scripts/data.py
@@ -6,3 +6,17 @@ folder_List = [
     ('Programmers', '프로그래머스'),
     ('SAMSUNG_SW_Expert_Academy', 'SAMSUNG SW Expert Academy')
 ]
+
+ReadmeTableHeader   = ["난이도","문제 링크","해결한 문제","전체 문제", "해결비율(%)"]
+LevelTableHeader    = ["순번","문제 유형","언어","문제 이름","문제 풀이","풀이 링크"]
+
+# 문제 링크가 없을 땐, 아래에 데이터를 추가해주세요.
+ProblemLinkBool = {
+    'SAMSUNG_SW_Expert_Academy': False
+}
+
+# 사용하는 언어에 따라, 아래의 dict을 추가해주세요.
+LangList = { # 확장자 : 언어이름
+    "py"    : "Python",
+    "java"  : "Java",
+}
