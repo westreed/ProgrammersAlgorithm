4647c914a106f7e5bc27c5a681f59b7a968640bf
JangSeHun <westreed@naver.com>
2023-02-04 22:27:48 +0900

    문제 업로드

---
+++ BAEKJOON/2Silver/IOIOI.py
@@ -0,0 +1,28 @@
+# 문자열
+# https://www.acmicpc.net/problem/5525
+
+# 입력
+'''
+1
+13
+OOIOIOIOIIIOI
+'''
+
+N = int(input())
+M = int(input())
+String = input().rstrip()
+
+A = 0
+Idx = 1
+Pat = 0
+while Idx < M:
+    if Idx < M-1 and String[Idx-1] == "I" and \
+    String[Idx] == "O" and String[Idx+1] == "I":
+        Pat += 1
+        Idx += 1
+    else:
+        if Pat >= N: A += Pat + 1 - N
+        Pat = 0
+    Idx += 1
+
+print(A)
