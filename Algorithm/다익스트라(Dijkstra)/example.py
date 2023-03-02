6d0db779988161839522643f80ef98f747bfef82
JangSeHun <westreed@naver.com>
2023-03-02 16:15:35 +0900

    다익스트라

---
+++ Algorithm/다익스트라(Dijkstra)/example.py
@@ -0,0 +1,25 @@
+INF = int(1e9)
+N = 6
+Graph = {0:[(1,3), (2,2), (3,5)], 1:[(2,2), (4,8)], 2:[(3,2)], 3:[(4,6)], 4:[(5,1)], 5:[]}
+Dist = [INF] * N
+
+def dijkstra(start):
+    import heapq
+    # (비용, 정점) 을 큐에 추가
+    queue = [(0, start)]
+    Dist[start] = 0
+
+    while queue:
+        dist, node = heapq.heappop(queue)
+
+        # 저장된 거리보다 크면 넘기기
+        if Dist[node] < dist: continue
+
+        for nxt, cost in Graph[node]:
+            _cost = Dist[node]+cost
+            if Dist[nxt] <= _cost: continue
+            Dist[nxt] = _cost
+            heapq.heappush(queue, (Dist[nxt], nxt))
+
+dijkstra(0)
+print(Dist)
