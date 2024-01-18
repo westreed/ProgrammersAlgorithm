3b784540c4d1c6463f41633ac3b8763d2674281a
JangSeHun <westreed@naver.com>
2024-01-18 18:49:48 +0900

    리코쳇 로봇 풀이

--- Programmers/Level2/리코쳇 로봇.py
+++ Programmers/Level2/리코쳇 로봇.py
@@ -1,60 +1,64 @@
 # 연습문제
 # https://school.programmers.co.kr/learn/courses/30/lessons/169199
 
-def solution(board:list):
-    start = None
-    goal  = None
-    SIZEX = len(board[0])+2
-    SIZEY = len(board)+2
-    board.insert(0, 'D'*SIZEX)
-    board.append('D'*SIZEX)
-    for y in range(1,SIZEY-1):
-        board[y] = 'D' + board[y] + 'D'
+def find_point_pos(board,N,M):
+    start, goal = None, None
+    for n in range(N):
+        for m in range(M):
+            if board[n][m] == 'R':
+                start = (n,m)
+            elif board[n][m] == 'G':
+                goal = (n,m)
+            if start is not None\
+                and goal is not None:
+                return start, goal
 
-    for y in range(SIZEY):
-        for x in range(SIZEX):
-            if board[y][x] == 'R':
-                start = (x, y)
-            elif board[y][x] == 'G':
-                goal = (x, y)
-                if  0 < x < SIZEX and \
-                    0 < y < SIZEY and \
-                    board[y-1][x] != 'D' and \
-                    board[y+1][x] != 'D' and \
-                    board[y][x-1] != 'D' and \
-                    board[y][x+1] != 'D':
-                    return -1
+def bfs(board,N,M,start,goal):
+    from collections import deque
 
-    # 한쪽이 막혀있으면 해당방향은 제외해야 하고,
-    # 이전에 실행했던 방향 역시 제외해야한다.
+    visit = [[0] * M for _ in range(N)]
+    print(visit)
+    queue = deque()
+    queue.append((start, -1, 0))
+    # U1 D2 L4 R8
+    arrow = ((0, -1, 0), (1, 1, 0), (2, 0, -1), (3, 0, 1))
+    reverse = (0, 0, 1, 1, -1)
 
-    def bfs(startPos, goalPos):
-        from collections import deque
-        # DebugText = ['위', '아래', '왼쪽', '오른쪽']
-        Arrow = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-        SkipA = {-1:-1, 0:0, 1:0, 2:1, 3:1}
-        Queue = deque()
-        # 현재위치, 움직인방향, 행동횟수
-        Queue.append((startPos, -1, 0))
-        while Queue:
-            pos,aro,cnt = Queue.popleft()
-            if pos == goalPos:
-                return cnt
+    while queue:
+        pos, d, cnt = queue.popleft()
+
+        if pos == goal:
+            for b in board:
+                print(b)
+            for v in visit:
+                print(v)
+            return cnt
+        
+        for _d, dx, dy in arrow:
+            nx,ny = pos
+            if reverse[_d] == reverse[d]: continue
+
+            while True:
+                _nx,_ny = nx+dx, ny+dy
+                if 0 > _nx or _nx >= N: break
+                if 0 > _ny or _ny >= M: break
+                if board[_nx][_ny] == "D": break
+                nx,ny = _nx,_ny
             
-            for d, arr in enumerate(Arrow):
-                if aro == d: continue
-                if SkipA[aro] == SkipA[d]: continue
+            if visit[nx][ny] & (1 << _d): continue
+            visit[nx][ny] += 1 << _d
+            queue.append(((nx,ny), _d, cnt+1))
+    
+    return -1
 
-                _nx, _ny = pos
-                ax, ay = arr
-                nx, ny = _nx + ax, _ny + ay
-                while board[ny][nx] != 'D':
-                    _nx, _ny = nx, ny
-                    nx, ny = _nx + ax, _ny + ay
-                if (_nx, _ny) == pos: continue
-                Queue.append(((_nx,_ny), d, cnt+1))
-        return -1
-    return bfs(start, goal)
+
+
+def solution(board):
+    N,M = len(board), len(board[0])
+    start, goal = find_point_pos(board,N,M)
+
+    return bfs(board,N,M,start,goal)
+
 
 
 board = [
